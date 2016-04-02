9d58921ce01e1534b742bf19273931ed85282726
codemonkey <steve@thegriffgroup.com>
2016-04-01 21:38:09 -0500

    Added rules file

---
+++ rules.js
@@ -0,0 +1,309 @@
+/* exported JuggRules */
+var JuggRules = {
+    Rules:{
+        10: {//10's phase
+            Acumulation: {
+                1: {
+                    reps: 10, 
+                    percent: 0.6
+                },
+                2: {
+                    reps: 10, 
+                    percent: 0.6
+                },
+                3: {
+                    reps: 10, 
+                    percent: 0.6
+                },
+                4: {
+                    reps: 10, 
+                    percent: 0.6
+                },
+                5: {
+                    reps: 10, 
+                    percent: 0.6
+                }
+            },
+            Intensification: {
+                1: {
+                    reps: 5, 
+                    percent: 0.55
+                },
+                2: {
+                    reps: 5, 
+                    percent: 0.625
+                },
+                3: {
+                    reps: 10, 
+                    percent: 0.675
+                },
+                4: {
+                    reps: 10, 
+                    percent: 0.675
+                },
+                5: {
+                    reps: 10, 
+                    percent: 0.675
+                }
+            },
+            Realization: {
+                1: {
+                    reps: 5, 
+                    percent: 0.5
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.6
+                },
+                3: {
+                    reps: 1, 
+                    percent: 0.7
+                },
+                4: {
+                    reps: 0 /* 0 is AMAP */, 
+                    percent: 0.75
+                }
+            }
+        },
+        8: {//8's phase
+            Acumulation: {
+                1: {
+                    reps: 8, 
+                    percent: 0.65
+                },
+                2: {
+                    reps: 8, 
+                    percent: 0.65
+                },
+                3: {
+                    reps: 8, 
+                    percent: 0.65
+                },
+                4: {
+                    reps: 8, 
+                    percent: 0.65
+                },
+                5: {
+                    reps: 8, 
+                    percent: 0.65
+                }
+            },
+            Intensification: {
+                1: {
+                    reps: 3, 
+                    percent: 0.6
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.675
+                },
+                3: {
+                    reps: 8, 
+                    percent: 0.725
+                },
+                4: {
+                    reps: 8, 
+                    percent: 0.725
+                },
+                5: {
+                    reps: 8, 
+                    percent: 0.725
+                }
+            },
+            Realization: {
+                1: {
+                    reps: 5, 
+                    percent: 0.5
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.6
+                },
+                3: {
+                    reps: 2, 
+                    percent: 0.7
+                },
+                4: {
+                    reps: 1, 
+                    percent: 0.75
+                },
+                5: {
+                    reps: 0 /* 0 is AMAP */, 
+                    percent: 0.8
+                }
+            }
+        },
+        5: {//5's phase
+            Acumulation: {
+                1: {
+                    reps: 5, 
+                    percent: 0.7
+                },
+                2: {
+                    reps: 5, 
+                    percent: 0.7
+                },
+                3: {
+                    reps: 5, 
+                    percent: 0.7
+                },
+                4: {
+                    reps: 5, 
+                    percent: 0.7
+                },
+                5: {
+                    reps: 5, 
+                    percent: 0.7
+                },
+                6: {
+                    reps: 5, 
+                    percent: 0.7
+                }
+            },
+            Intensification: {
+                1: {
+                    reps: 2, 
+                    percent: 0.65
+                },
+                2: {
+                    reps: 2, 
+                    percent: 0.725
+                },
+                3: {
+                    reps: 5, 
+                    percent: 0.775
+                },
+                4: {
+                    reps: 5, 
+                    percent: 0.775
+                },
+                5: {
+                    reps: 5, 
+                    percent: 0.775
+                },
+                6: {
+                    reps: 5, 
+                    percent: 0.775
+                }
+            },
+            Realization: {
+                1: {
+                    reps: 5, 
+                    percent: 0.5
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.6
+                },
+                3: {
+                    reps: 2, 
+                    percent: 0.7
+                },
+                4: {
+                    reps: 1 , 
+                    percent: 0.75
+                },
+                5: {
+                    reps: 1 , 
+                    percent: 0.8
+                },
+                6: {
+                    reps: 0 , 
+                    percent: 0.85
+                }
+            }
+        },
+        3: {//3's phase
+            Acumulation: {
+                1: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                3: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                4: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                5: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                6: {
+                    reps: 3, 
+                    percent: 0.75
+                },
+                7: {
+                    reps: 3, 
+                    percent: 0.75
+                }
+            },
+            Intensification: {
+                1: {
+                    reps: 1, 
+                    percent: 0.7
+                },
+                2: {
+                    reps: 1, 
+                    percent: 0.775
+                },
+                3: {
+                    reps: 3, 
+                    percent: 0.825
+                },
+                4: {
+                    reps: 3, 
+                    percent: 0.825
+                },
+                5: {
+                    reps: 3, 
+                    percent: 0.825
+                },
+                6: {
+                    reps: 3, 
+                    percent: 0.825
+                },
+                7: {
+                    reps: 3, 
+                    percent: 0.825
+                }
+            },
+            Realization: {
+                1: {
+                    reps: 5, 
+                    percent: 0.5
+                },
+                2: {
+                    reps: 3, 
+                    percent: 0.6
+                },
+                3: {
+                    reps: 2, 
+                    percent: 0.7
+                },
+                4: {
+                    reps: 1, 
+                    percent: 0.75
+                },
+                5: {
+                    reps: 1, 
+                    percent: 0.8
+                },
+                6: {
+                    reps: 1, 
+                    percent: 0.85
+                },
+                7: {
+                    reps: 0, 
+                    percent: 0.9}
+            }
+        }
+    }
+
+};
